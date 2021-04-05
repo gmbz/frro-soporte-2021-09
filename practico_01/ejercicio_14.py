e37a813e3a2f23d0b9b5d6156a6be7a326ecbbbd
Gonzalo Martin Bermejo Zambrini <gbermejozambrini@gmail.com>
2021-04-04 21:43:34 -0300

    Resolucion de la primer parte

--- practico_01/ejercicio_14.py
+++ practico_01/ejercicio_14.py
@@ -15,7 +15,7 @@ from typing import Callable, Iterable
 def apply(lista: Iterable[int], func: Callable[[int], bool]) -> Iterable[bool]:
     """Toma una lista y una función que toma un parámetro y devuelve una lista
     con la función aplicada a todos los elementos."""
-    pass # Completar
+    return list(map(func(lista)))
 
 
 # NO MODIFICAR - INICIO
@@ -34,7 +34,7 @@ Referencia: https://docs.python.org/3/library/functools.html#functools.partial
 lista = [3, 4, 5, 6, 7, 8]
 min_ = 4
 max_ = 7
-nueva_funcion = # Completar
+nueva_funcion = partial(esta_entre_valores, [x for x in lista], min_=min_, max_=max_)
 
 # NO MODIFICAR - INICIO
 lista = [3, 4, 5, 6, 7, 8]
